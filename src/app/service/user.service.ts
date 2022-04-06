d61d5db087fbf79ce12fe2f5d305c821573c4967
jomile.kundrotaite@gmail.com <jomile>
2022-04-06 19:04:07 +0300

    feat_login_page

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,17 @@
+import { UsersInterface } from './../../assets/data/Data';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  loadUsers(): Observable<UsersInterface[]> {
+    return this.http.get<UsersInterface[]>('./assets/data/DATA.json');
+  }
+}
---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,25 @@
+import { UsersInterface } from './../../assets/data/Data';
+import { Component, OnInit } from '@angular/core';
+import { map, Observable, of, tap } from 'rxjs';
+import { UserService } from '../service/user.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss']
+})
+export class UserListComponent implements OnInit {
+  users: UsersInterface[] = [];
+
+  constructor(private _userService: UserService) { }
+
+  ngOnInit(): void {
+    this._userService.loadUsers().subscribe(
+      (data: UsersInterface[]) => { this.users = data }
+    );
+  }
+
+
+
+
+}
