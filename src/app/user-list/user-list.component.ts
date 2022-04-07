875ec91fffa1e81b66afdcfa682564d8a55cf752
Ausra <ausra.urnezaite@gmail.com>
2022-04-07 13:43:01 +0300

    html users

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -6,7 +6,7 @@ import { UserService } from '../service/user.service';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  // styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
   users: UsersInterface[] = [];
