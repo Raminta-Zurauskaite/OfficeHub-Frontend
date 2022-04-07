f77a8d596318e0cf6f35dd8b5076a7a59874207e
Ausra <ausra.urnezaite@gmail.com>
2022-04-07 13:55:52 +0300

    city-list component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CityListComponent } from './city-list/city-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    CityListComponent
   ],
   imports: [
     BrowserModule,
