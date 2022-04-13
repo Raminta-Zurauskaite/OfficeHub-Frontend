5e0c598cb39c96c676772e57994bfba8d1d82818
jomile.kundrotaite@gmail.com <jomile>
2022-04-13 19:19:01 +0300

    feat prettier

--- src/app/floor-plan/floor-plan.component.ts
+++ src/app/floor-plan/floor-plan.component.ts
@@ -1,33 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { DeskInterface } from 'src/assets/data/Desks';
-import { DataService } from '../service/data/data.service';
-import { Observable, of } from 'rxjs';
+import { Component, OnInit } from '@angular/core'
+import { FormControl, Validators } from '@angular/forms'
+import { DeskInterface } from 'src/assets/data/Desks'
+import { DataService } from '../service/data/data.service'
+import { Observable, of } from 'rxjs'
 
 @Component({
-  selector: 'app-floor-plan',
-  templateUrl: './floor-plan.component.html',
-  styleUrls: ['./floor-plan.component.scss']
+    selector: 'app-floor-plan',
+    templateUrl: './floor-plan.component.html',
+    styleUrls: ['./floor-plan.component.scss'],
 })
 export class FloorPlanComponent implements OnInit {
-  myThumbnail = "assets/images/20-floor.png";
-  myFullresImage = "assets/images/20-floor.png";
-  desks$: Observable<DeskInterface[]> = of();
+    myThumbnail = 'assets/images/20-floor.png'
+    myFullresImage = 'assets/images/20-floor.png'
+    desks$: Observable<DeskInterface[]> = of()
 
-  selectedDate = new Date();
-  selectedDesk = new FormControl('', [Validators.required]);
+    selectedDate = new Date()
+    selectedDesk = new FormControl('', [Validators.required])
 
-  constructor(private dataService: DataService) { }
+    constructor(private dataService: DataService) {}
 
-  ngOnInit(): void {
-    this.desks$ = this.dataService.loadDesks();
+    ngOnInit(): void {
+        this.desks$ = this.dataService.loadDesks()
+    }
 
-  }
-
-  onSubmit() {
-    localStorage.setItem("deskId", this.selectedDesk.value);
-    localStorage.setItem("booking_date", this.selectedDate.toISOString().slice(0, 10));
-  }
-
-
+    onSubmit() {
+        localStorage.setItem('deskId', this.selectedDesk.value)
+        localStorage.setItem(
+            'booking_date',
+            this.selectedDate.toISOString().slice(0, 10)
+        )
+    }
 }
