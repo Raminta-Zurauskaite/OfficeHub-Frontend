8d3b49837ca41e91a1499cac390bf3a4048efcc3
jomile.kundrotaite@gmail.com <jomile>
2022-04-27 18:34:29 +0300

    feat background page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,20 +4,22 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  userID = Number(localStorage.getItem('user'));
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.userID);
+  }
 
   isLoggedIn() {
     const loggedInUser = localStorage.getItem('user');
     if (loggedInUser) {
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   }
